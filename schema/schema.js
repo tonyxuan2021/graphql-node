482675f82b1e695f3056cf84c25bbb2bc8f9dc2a
tonyxuan2021 <tonyyexuan@gmail.com>
2023-02-23 18:12:55 -0800

    added deep circular relasions with company and user

--- schema/schema.js
+++ schema/schema.js
@@ -1,14 +1,47 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+} = graphql;
 const axios = require('axios');
+
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  // arrow function is used to prevent circular referencing error
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      //since 1 company can have many users
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((res) => res.data);
+      },
+    },
+  }),
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
-  },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then((res) => res.data);
+      },
+    },
+  }),
 });
 
 // RootQuery is used to return a very particular user
@@ -22,6 +55,15 @@ const RootQuery = new GraphQLObjectType({
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
           .then((response) => response.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
+          .then((resp) => resp.data);
       },
     },
   },
